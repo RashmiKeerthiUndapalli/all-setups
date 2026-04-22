c7107883622b183f24e634f17a657ae2635c8144
Rashmi <84272096+RashmiKeerthiUndapalli@users.noreply.github.com>
2026-04-22 18:49:26 +0530

    Upgrade Java version in jenkins.sh
    Updated Jenkins installation script to use Java 21 instead of Java 17.

--- jenkins.sh
+++ jenkins.sh
@@ -2,7 +2,7 @@ sudo yum update –y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade
-sudo yum install java-17-amazon-corretto -y
+sudo yum install java-21-amazon-corretto java-21-amazon-corretto-devel -y
 sudo yum install jenkins git -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
@@ -14,3 +14,4 @@ echo '/var/tmp_disk /tmp none bind 0 0' | sudo tee -a /etc/fstab
 sudo systemctl mask tmp.mount
 df -h /tmp
 sudo systemctl restart jenkins
+sudo yum install java-17-amazon-corretto -y
